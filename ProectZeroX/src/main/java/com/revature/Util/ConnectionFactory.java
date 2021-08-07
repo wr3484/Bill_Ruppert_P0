ecc5c8ade4a6c3d041f1fc2930bc82a6e38ba513
wr3484 <wruppert545551@gmail.com>
2021-08-07 19:31:56 -0400

    Added Documentation, cleaned up comments

--- ProectZeroX/src/main/java/com/revature/Util/ConnectionFactory.java
+++ ProectZeroX/src/main/java/com/revature/Util/ConnectionFactory.java
@@ -1,37 +1,29 @@
 package com.revature.Util;
 
 import java.sql.*;
-//import java.beans.Statement;
-import java.sql.Statement;
 import java.sql.Connection;
-import java.sql.ResultSet;
-import java.util.ArrayList;
 import java.sql.DriverManager;
 
 public class ConnectionFactory {
-    // This will create a connection with the DB without having to hardcode
-
-
+    /**
+     * This will create a connection with the DB without having to hardcode
+     * Anytime the connection is needed this class can be called
+     * @return
+     * @throws SQLException
+     */
 
     public static Connection getConnection() throws SQLException {
         // Obtain a connection through JDBC
         Connection conn = null;
 
+        // Connection locates the Environment Variables for JDBC
         conn = DriverManager.getConnection(
                 System.getenv("db_url"),System.getenv("db_name"),
                         System.getenv("db_password"));
 
-
         return conn;
     }
-
-//    List<Planet> dbList = new ArrayList<>();
-//
-//    String sql = "Select * from accountinformation";
-//
-//    Statement s = f.createStatement();
-//
-//    ResultSet rs = s.executeQuery(sql);
+    
 }
 
 
